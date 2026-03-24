cc035397ad76d34bf13895a27a95ba74fb3b63d5
msbizrise-arch <msbizrise@gmail.com>
2026-03-24 18:59:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "38498066"
 api_hash = "c9696114751feacdeb1b4487f5839a1a"
-bot_token = "8277629583:AAHtb7rCNXKk1nhJe_GpR3bLp8nb9jh0r_w"
+bot_token = "8534420363:AAGFsIzriIXeMGWTeprqEBG5zzLSU09ZqZk"
 auth_users = [8703802029]
