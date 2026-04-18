753d9b3674f1f3439cdce9479cff494761a0cf20
msbro-rmw <mdinabano189@gmail.com>
2026-04-18 20:08:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = 38498066
 api_hash = "c9696114751feacdeb1b4487f5839a1a"
-bot_token = "8534420363:AAGuEuR5FLLSqwG9nOYMirLD3eso2N3pPdM"
+bot_token = "8534420363:AAGwYoB8j5mBM2nL7CrZno34c_qARTMZBKU"
 auth_users = [8703802029]
